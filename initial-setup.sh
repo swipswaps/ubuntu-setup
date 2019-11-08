a59075f77a1bae551fe9a2e6eb29b95be72abc7f
Florian Hübner <fhuebner@posteo.de>
2019-11-08 14:10:33 +0100

    refactor

--- initial-setup.sh
+++ initial-setup.sh
@@ -3,8 +3,13 @@
 ## AUTHOR: Florian Hübner (fhuebner@posteo.de)
 ## URL: https://github.com/huebnerf/ubuntu-setup
 
+
+# User will be created if not existing
 export SETUP_USER='' # <- add username here
+
+# If no key is supplied, user password will be set at runtime and ssh password login will be activated
 export SETUP_SSHKEY='' # <- add ssh pubkey here
+
 
 ########
 
@@ -16,54 +21,56 @@ then
   exit
 fi
 
-
 if ! grep -q DISTRIB_CODENAME=bionic /etc/lsb-release;
 then
   echo " → This script is intended to be run on Ubuntu 18.04 LTS Bionic Beaver!";
   exit;
 fi
 
-
 if [ -z "$SETUP_USER" ];
 then
-  echo " → Please supply a Username at SETUP_USER!"
+  echo " → Please supply a username at SETUP_USER!"
   exit;
 fi
-
 
 if [ -z "$SETUP_SSHKEY" ];
 then
   echo " → NO SSH KEY SUPPLIED, PASSWORD LOGIN WILL BE ENABLED!"
 fi
 
-
 export DEBIAN_FRONTEND=noninteractive
 
 
-# setup user
-if id "$SETUP_USER" >/dev/null 2>&1;
+# setup admin user
+if id "$SETUP_USER";
 then
-  echo " → user already exists"
+  echo " → User $SETUP_USER already exists"
 else
-  echo " → adding user"
+  echo " → Adding user $SETUP_USER"
   adduser --quiet --disabled-password --gecos "" "$SETUP_USER"
   gpasswd -a "$SETUP_USER" sudo
+  if [ -z "$SETUP_SSHKEY" ];
+  then
+  echo " → Plase specify a password for $SETUP_USER"
+    passwd "$SETUP_USER"
+  fi
 fi
 
 
 # use default apt archive servers
-echo " → configuring default apt server"
+echo " → Configuring default apt server"
 {
   echo "deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse"
-  echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multierse"
+  echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse"
   echo "deb http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse"
   echo "deb http://archive.ubuntu.com/ubuntu bionic-backports main restricted universe multiverse"
 } > /etc/apt/sources.list
+apt-get -qy update
 
 
 # remove bloat
-echo " → removing bloat"
-apt-get -qy autoremove --purge cloud-init snapd
+echo " → Removing bloat"
+apt-get -qy autoremove --purge cloud-init landscape-common pastebinit popularity-contest snapd
 # cloud-init
 echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 # snapd
@@ -71,14 +78,13 @@ rm -rf /var/cache/snapd/
 rm -rf /root/snap
 
 
-# update packages
-echo " → updating apt packages"
-apt-get -qy update
+# update apt packages
+echo " → Updating apt packages"
 apt-get -qy upgrade
 
 
 # set up locales
-echo " → configuring locales"
+echo " → Configuring locales"
 apt-get -qy install language-pack-en-base
 timedatectl set-timezone Europe/Berlin
 export LC_ALL='en_US.UTF-8'
@@ -86,8 +92,8 @@ export LANG='en_US.UTF-8'
 update-locale LC_ALL="en_GB.UTF-8" LANG="en_GB.UTF-8"
 
 
-# install packages
-echo " → installing additional apt packages"
+# install additional apt packages
+echo " → Installing additional apt packages"
 apt-get -qy install  apt-utils              \
                      autoconf               \
                      automake               \
@@ -97,7 +103,7 @@ apt-get -qy install  apt-utils              \
                      curl                   \
                      fail2ban               \
                      git                    \
-                     gnuplot                \
+                     gnupg2                 \
                      htop                   \
                      iftop                  \
                      make                   \
@@ -108,7 +114,6 @@ apt-get -qy install  apt-utils              \
                      rsync                  \
                      screen                 \
                      shellcheck             \
-                     sudo                   \
                      tldr                   \
                      tree                   \
                      ufw                    \
@@ -116,74 +121,87 @@ apt-get -qy install  apt-utils              \
                      wget
 
 
-# clean up
-echo " → cleanup apt"
+# clean up apt
+echo " → Cleaning up apt"
 apt-get -qy clean
 apt-get -qy autoremove
 
 
 # ssh config
-echo " → configuring ssh"
-cat <<'EOF' > /etc/ssh/ssh_config
-HashKnownHosts yes
-HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa,ecdsa-sha2-nistp521-cert-v01@openssh.com,ecdsa-sha2-nistp384-cert-v01@openssh.com,ecdsa-sha2-nistp256-cert-v01@openssh.com,ecdsa-sha2-nistp521,ecdsa-sha2-nistp384,ecdsa-sha2-nistp256
-KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
-MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
-Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-EOF
+echo " → Configuring ssh"
+{
+  echo "HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa,ecdsa-sha2-nistp521-cert-v01@openssh.com,ecdsa-sha2-nistp384-cert-v01@openssh.com,ecdsa-sha2-nistp256-cert-v01@openssh.com,ecdsa-sha2-nistp521,ecdsa-sha2-nistp384,ecdsa-sha2-nistp256"
+  echo "KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256"
+  echo "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com"
+  echo "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr"
+  echo "HashKnownHosts yes"
+} > /etc/ssh/ssh_config
 
 
 # sshd config
-echo " → configuring sshd"
-cat <<'EOF' > /etc/ssh/sshd_config
-HostKey /etc/ssh/ssh_host_ed25519_key
-HostKey /etc/ssh/ssh_host_rsa_key
-
-KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com
-Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com
-
-AuthenticationMethods publickey
-PermitRootLogin No
-PasswordAuthentication no
-PermitEmptyPasswords no
-ChallengeResponseAuthentication no
-GSSAPIAuthentication no
-EOF
+echo " → Configuring sshd"
+{
+  echo "HostKey /etc/ssh/ssh_host_ed25519_key"
+  echo "HostKey /etc/ssh/ssh_host_rsa_key"
+  echo "KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256"
+  echo "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com"
+  echo "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com"
+  echo "LogLevel VERBOSE"
+  echo "Subsystem sftp  /usr/lib/ssh/sftp-server -f AUTHPRIV -l INFO"
+  echo "PermitRootLogin No"
+  echo "LoginGraceTime 1m"
+  echo "UseDNS no"
+  echo "AllowTcpForwarding no"
+  echo "X11Forwarding no"
+  echo "AuthenticationMethods publickey"
+  echo "UsePAM yes"
+  echo "PasswordAuthentication no"
+  echo "PermitEmptyPasswords no"
+  echo "ChallengeResponseAuthentication no"
+  echo "KerberosAuthentication no"
+  echo "GSSAPIAuthentication no"
+  echo "Match User $SETUP_USER"
+  echo "    AllowTcpForwarding yes"
+} > /etc/ssh/sshd_config
 
 
 # configure ssh auth
 if [[ -n "$SETUP_SSHKEY" ]];
 then
-  echo " → authorizing ssh key"
+  echo " → Authorizing ssh key"
   cd /home/"$SETUP_USER"
   mkdir -p .ssh
   touch .ssh/authorized_keys .ssh/known_hosts
-  echo "'$SETUP_SSHKEY'" > .ssh/authorized_keys
+  echo "$SETUP_SSHKEY" > .ssh/authorized_keys
   chmod 700 .ssh
   chmod 600 .ssh/authorized_keys
   chown -R "$SETUP_USER":"$SETUP_USER" .ssh
 else
-  echo " → ENABLING SSH PASSWORD AUTH!"
-  sed -i "/AuthenticationMethods/s/publickey/publickey password/g" /etc/ssh/sshd_config
+  echo " → Enabling ssh password auth for $SETUP_USER"
+  {
+    echo "    AuthenticationMethods publickey password"
+    echo "    PasswordAuthentication yes"
+  } >> /etc/ssh/sshd_config
 fi
 
 
 # generate server keys
-echo " → generating ssh server keys"
+echo " → Deleting old ssh server keys"
 cd /etc/ssh
 shred -u ssh_host_*key*
+echo " → Generating ed25519 ssh server key"
 ssh-keygen -t ed25519 -f ssh_host_ed25519_key -N ''
+echo " → Generating rsa ssh server key"
 ssh-keygen -t rsa -b 8192 -f ssh_host_rsa_key -N ''
 
 
 # restart sshd
-echo " → restarting sshd"
+echo " → Restarting sshd"
 systemctl restart sshd
 
 
 # configure firewall
-echo " → configuring firewall"
+echo " → Setting up firewall"
 ufw logging on
 ufw default deny incoming
 ufw default allow outgoing
